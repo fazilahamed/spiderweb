324b2f83b875464b31b08b38091822082692ec67
fazil hussain <fazilahamedh@gmail.com>
2011-06-16 19:57:18 +0200

    added ola, removed class file

--- CDKtests/src/test/Helloworld.java
+++ CDKtests/src/test/Helloworld.java
@@ -8,7 +8,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("hello world");
+		System.out.println("hello ola world");
 	}
 
 }
