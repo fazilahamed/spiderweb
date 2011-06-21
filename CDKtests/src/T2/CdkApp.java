8f03073c21202e7d85ed0b6b3aa72cab9051e5a7
fazil hussain <fazilahamedh@gmail.com>
2011-06-21 20:16:43 +0200

    Added CDKApp

---
+++ CDKtests/src/T2/CdkApp.java
@@ -0,0 +1,13 @@
+package T2;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.Atom;
+
+public class CdkApp {
+
+	public static void main(String[] args) {
+			IAtom atom = new Atom("C");
+			System.out.println(atom);
+			
+	}
+
+}
