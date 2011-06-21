ab333d98d69a74e7694c785f7b5917c5a99bee43
fazil hussain <fazilahamedh@gmail.com>
2011-06-21 20:34:10 +0200

    cjhanged cdkapp

--- CDKtests/src/T2/CdkApp.java
+++ CDKtests/src/T2/CdkApp.java
@@ -1,13 +1,18 @@
 package T2;
 import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.templates.MoleculeFactory;
 import org.openscience.cdk.Atom;
+import org.openscience.cdk.Molecule;
 
 public class CdkApp {
 
 	public static void main(String[] args) {
-			IAtom atom = new Atom("N");
-			System.out.println(atom);
-			
+		
+		Molecule benzene = MoleculeFactory.makeBenzene();
+		for (IAtom atom : benzene.atoms()){
+			System.out.println("Atom: " + atom);
+		}
+					
 	}
 
 }
